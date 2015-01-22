10d95d54280b1c42138392b41ea2256c94525f17
Nish <lalgee.nish@gmail.com>
2015-01-22 16:15:59 -0500

    Edit models - remove attr_accessible

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-	attr_accessible :name
+	#attr_accessible :name (attr_accessible IS NO LONGER USED IN RAILS 4+)
 	has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-attr_accessible :title, :body, :category_id, :author_id
+	#attr_accessible :title, :body, :category_id, :author_id (attr_accessible IS NO LONGER USED IN RAILS 4+)
 	belongs_to :category
 end
